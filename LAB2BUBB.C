16a35ecef37aa4a92c5ddd6a611b29b2332317d9
Alok R <alokrmysore@gmail.com>
2026-01-29 12:10:41 +0530

    Add files via upload

---
+++ LAB2BUBB.C
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+#include<time.h>
+void main(){
+int n,i,j,temp,t;
+int ar[10];
+clrscr();
+n=10;
+    // Seed the random number generator
+    srand(time(NULL));
+
+    // Fill array with random numbers (0–99)
+    for ( i = 0; i < n; i++) {
+	ar[i] = rand() % 100;
+    }
+
+    // Print the array
+    for (i = 0; i < n; i++) {
+	printf("%d ", ar[i]);
+    }
+n=10;
+t=0;
+printf("array after sort \n");
+for(i=0;i<n;i++){
+for(j=0;j<n;j++){
+t=t+3;
+if(ar[i]>ar[j]){
+t=t+3;
+temp=ar[j];
+ar[j]=ar[j+1];
+ar[j+1]=temp;
+t=t+3;
+
+}}
+}
+for(i=0;i<n;i++){
+printf("%d \t",ar[i]);
+}
+printf("time complexity is %d",t);
+getch();
+}
+
+
